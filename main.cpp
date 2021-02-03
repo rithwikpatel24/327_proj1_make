5f91c261abc9d558b2a61fab3b864fb0b1021aa6
rithwikpatel24 <rithwik.patel.18@cnu.edu>
2021-02-03 16:47:37 -0500

    Project1

--- main.cpp
+++ main.cpp
@@ -1,11 +1,7 @@
 //#includes are ALWAYS at top of file
-#include <iostream>	//iostream is where std::cout and std::endl are defined
-
-std::string func()
-{
-	//say hello
-	return "hello";
-}
+#include "myfunc.h"
+#include <iostream>
+	//iostream is where std::cout and std::endl are defined
 
 int main()
 {
